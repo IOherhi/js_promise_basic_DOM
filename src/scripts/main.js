eaf6d123327da11eed10033fad4be7fac9d1a5d1
Ivan Herhi <ionherhi@gmail.com>
2025-03-23 11:12:45 +0100

    promis01

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,30 @@
 'use strict';
 
-// write your code here
+const logo = document.querySelector('.logo');
+
+let isResolved = false;
+
+const promis01 = new Promise((resolve, reject) => {
+  logo.addEventListener('click', (e) => {
+    isResolved = true;
+    resolve();
+  });
+});
+
+promis01.then(() => {
+  logo.classList.add('message');
+  logo.textContent = 'Promise was resolved!';
+});
+
+const promis02 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (!isResolved) {
+      reject(new Error("Promise was rejected because of timeout"));
+    }
+  }, 2000);
+});
+
+promis02.catch(() => {
+  logo.classList.add('message error-message');
+  logo.textContent = 'Promise was rejected!';
+});
